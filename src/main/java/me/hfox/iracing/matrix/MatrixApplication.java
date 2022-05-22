b5a0f8a6eebed1016243bdeafc777e076074508c
Harry Fox <1254033+hfoxy@users.noreply.github.com>
2022-05-22 01:21:02 +0100

    Add basic stuff

--- src/main/java/me/hfox/iracing/matrix/MatrixApplication.java
+++ src/main/java/me/hfox/iracing/matrix/MatrixApplication.java
@@ -1,4 +1,17 @@
 package me.hfox.iracing.matrix;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
 public class MatrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(MatrixApplication.class);
+        builder.bannerMode(Banner.Mode.OFF);
+        ConfigurableApplicationContext app = builder.run(args);
+    }
+
 }
